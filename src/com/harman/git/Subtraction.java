484baf11b9f463d26e7ffb2badd9318bccb2ea4d
Muheesh <muheeshas@gmail.com>
2022-03-20 19:45:30 +0530

    subtraction

---
+++ src/com/harman/git/Subtraction.java
@@ -0,0 +1,11 @@
+package com.harman.git;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int num1,num2,num3;
+        num1=10;
+        num2=5;
+        num3=num1-num2;
+        System.out.println(num3);
+    }
+}
